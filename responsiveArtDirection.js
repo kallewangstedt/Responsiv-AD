5bcafbe331d728431976ae7a603f1d45a8e34beb
kallewangstedt <kalle@wangstedt.com>
2014-04-09 13:37:25 +0200

    Update responsiveArtDirection.js
    Bättre synk.

--- responsiveArtDirection.js
+++ responsiveArtDirection.js
@@ -1,16 +1,22 @@
+// Responsiv art direction
+// =======================
+
+// ###Dynamisk responsiv bildhantering via javascript
+
+
 // Funktion för att dynamiskt byta ut bilder på en sida så att de
 // anpassas till skärmstorleken. Alltså inte en traditionell omskalning,
 // utan gör det möjligt att ha helt olika bilder för olika skärmar.
 
-// Standardbilden anges i <img /> med attributet data-src-default och
+// Standardbilden anges i `<img />` med attributet `data-default-src` och
 // namnet på den används sedan för att hämta de anpassade bilderna.
 
 // Övriga bilder ska ligga bredvid standardbilden och vara namngivna enligt följande:
 
-// Standard:						bild.png
-// Högupplöst:						bild@2x.png
-// Specifik skärmstorlek:			bild@320.png
-// Högupplöst + skärmanpassning:	bild@320-2x.png
+//     Standard:                     bild.png
+//     Högupplöst:                   bild@2x.png
+//     Specifik skärmstorlek:        bild@320.png
+//     Högupplöst + skärmanpassning: bild@320-2x.png
 
 // Skärmstorleken som anges ska vara densamma som en traditionell mediaquery i CSS.
 
@@ -18,8 +24,17 @@
 // hand om WP:s namngivning också. Så länge alla bilder är döpta enligt ovan och
 // ligger i samma mapp på servern (har samma sökväg) så kommer det att fungera.
 
-// Lägg till alla skärmstorlekar som ska testas mot i variabeln screen_widths som poster
+// Lägg till alla skärmstorlekar som ska testas mot i variabeln `screen_widths` som poster
 // i en array med störst först och minst sist.
+
+// ###Hur gör man?
+
+// 1. Filen `responsiveArtDirection.js` är beroende av jQuery och skall laddas efter att jQuery laddats.
+// 2. Filen `responsive_art_direction.php` ska laddas i Wordpress i det aktiva temat och ändrar där bilder infogade i en artikel så att de stämmer överens med syntaxen för javascriptet.
+// 3. Vid användning i WP läggs sökvägen till en laddningsbild `spinner.gif` i alla bilder så att den visas innan skriptet byter ut den mot den responsiva bilden.
+// 4. Ändra variabeln `screen_widths` i javascriptet till att inkludera alla brytpunkter för skärmstorlekar som bilderna ska kunna anpassas till.
+
+// Lycka till!
 
 ;(function(responsiveArtDirection, $, undefined){
 
