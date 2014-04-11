75d247fe3a1a96b06010035b655dff2a40a421f7
kallewangstedt <kalle@wangstedt.com>
2014-04-11 11:23:12 +0200

    Update responsive_art_direction.php
    Bättre formatering.

--- responsive_art_direction.php
+++ responsive_art_direction.php
@@ -9,7 +9,7 @@ function responsive_art_direction(){
 
 	$spinner = 'src="' . get_bloginfo("stylesheet_directory") . "/spinner.gif" . '"';
 
-	return preg_replace('#(<img.+?)(src="[^"]*")(.*?/?>)#i', '$1data-default-$2 '.$spinner.' $3', $post->post_content);
+	return preg_replace('#(<img.+?)(src="[^"]*")(.*?/?>)#i', '$1data-default-$2 ' . $spinner . ' $3', $post->post_content);
 }
 add_filter("the_content", "responsive_art_direction", 9);
 
