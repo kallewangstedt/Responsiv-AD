16a86cf15a0d4ecf9df6a245c49ff7f8b503c409
kallewangstedt <kalle@wangstedt.com>
2014-04-11 11:22:31 +0200

    Update responsive_art_direction.php
    Filtret måste köras tidigare än standard (10).

--- responsive_art_direction.php
+++ responsive_art_direction.php
@@ -7,11 +7,11 @@ require_once("responsive_art_direction.php");
 function responsive_art_direction(){
 	global $post;
 
-	$spinner = 'src="'.get_bloginfo("stylesheet_directory") . "/spinner.gif".'"';
+	$spinner = 'src="' . get_bloginfo("stylesheet_directory") . "/spinner.gif".'"';
 
 	return preg_replace('#(<img.+?)(src="[^"]*")(.*?/?>)#i', '$1data-default-$2 '.$spinner.' $3', $post->post_content);
 }
-add_filter("the_content", "responsive_art_direction");
+add_filter("the_content", "responsive_art_direction", 9);
 
 
 ?>
