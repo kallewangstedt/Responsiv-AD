0d8bc8c1b580299837f42b0f488ab6552a72d8fc
kallewangstedt <kalle@wangstedt.com>
2014-04-11 23:00:39 +0200

    Update responsive_art_direction.php
    Bättre regex.

--- responsive_art_direction.php
+++ responsive_art_direction.php
@@ -9,7 +9,11 @@ function responsive_art_direction(){
 
 	$spinner = 'src="' . get_bloginfo("stylesheet_directory") . "/spinner.gif" . '"';
 
-	return preg_replace('#(<img.+?)(src="[^"]*")(.*?/?>)#i', '$1data-default-$2 ' . $spinner . ' $3', $post->post_content);
+	$img = preg_replace('#(<img.+?)(src="[^"]*")(.*?/?>)#i', '$1data-default-$2 ' . $spinner . '$3', $post->post_content);
+	$img = preg_replace('#(<img(.*?))(width="[^"]*"){1}(.*?/?>)#i', '$1$4', $img);
+	$img = preg_replace('#(<img(.*?))(height="[^"]*"){1}(.*?/?>)#i', '$1$4', $img);
+	return $img;
+
 }
 add_filter("the_content", "responsive_art_direction", 9);
 
