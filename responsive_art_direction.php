f4a781765fd961399854e258b89327cc94f3efb8
kallewangstedt <kalle@wangstedt.com>
2014-04-12 09:25:01 +0200

    Update responsive_art_direction.php
    Rätt filterordning.

--- responsive_art_direction.php
+++ responsive_art_direction.php
@@ -4,19 +4,17 @@ Lägg följande kod i functions.php eller gör en
 require_once("responsive_art_direction.php");
 */
 
-function responsive_art_direction(){
-	global $post;
+function responsive_art_direction($content){
 
 	$spinner = 'src="' . get_bloginfo("stylesheet_directory") . "/spinner.gif" . '"';
 
-	$img = preg_replace('#(<img.+?)(src="[^"]*")(.*?/?>)#i', '$1data-default-$2 ' . $spinner . '$3', $post->post_content);
+	$img = preg_replace('#(<img.*?)(src="[^"]*")(.*?/?>)#i', '$1data-default-$2 ' . $spinner . '$3', $content);
 	$img = preg_replace('#(<img(.*?))(width="[^"]*"){1}(.*?/?>)#i', '$1$4', $img);
 	$img = preg_replace('#(<img(.*?))(height="[^"]*"){1}(.*?/?>)#i', '$1$4', $img);
-	$img = preg_replace('#(<img(.*?))([ ]{2,}){1,}(.*?/?>)#i', '$1 $4', $img);
+	$img = preg_replace('/[ ]{2,}/i', ' ', $img);
 	return $img;
-
 }
-add_filter("the_content", "responsive_art_direction", 9);
+add_filter("the_content", "responsive_art_direction");
 
 
 ?>
