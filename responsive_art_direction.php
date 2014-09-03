972261fbe68e6a9d8fbc05a02407555ebab1fc35
kallewangstedt <kalle@wangstedt.com>
2014-09-03 14:53:11 +0200

    Update responsive_art_direction.php
    Lade till standardstorlekar.

--- responsive_art_direction.php
+++ responsive_art_direction.php
@@ -6,12 +6,13 @@ require_once("responsive_art_direction.php");
 
 function responsive_art_direction($content){
 
-	$spinner = 'src="' . get_bloginfo("stylesheet_directory") . "/spinner.gif" . '"';
+	$spinner = 'src="' . get_bloginfo("stylesheet_directory") . "/global/images/spinner.gif" . '"';
 
 	$img = preg_replace('#(<img.*?)(src="[^"]*")(.*?/?>)#i', '$1data-default-$2 ' . $spinner . '$3', $content);
-	$img = preg_replace('#(<img(.*?))(width="[^"]*"){1}(.*?/?>)#i', '$1$4', $img);
-	$img = preg_replace('#(<img(.*?))(height="[^"]*"){1}(.*?/?>)#i', '$1$4', $img);
+	$img = preg_replace('#(<img(.*?))(width="[^"]*"){1}(.*?/?>)#i', '$1data-default-$3$4', $img);
+	$img = preg_replace('#(<img(.*?))(height="[^"]*"){1}(.*?/?>)#i', '$1data-default-$3$4', $img);
 	$img = preg_replace('/[ ]{2,}/i', ' ', $img);
+
 	return $img;
 }
 add_filter("the_content", "responsive_art_direction");
